cd366bbec678c8ef7e0a2e819b88678824e9a222
Adam C <cohen.adam@epitech.eu>
2021-03-04 20:32:16 +0100

    feat: abacus fragment page

--- app/src/main/java/ca/ulaval/ima/tp2/ui/abacus/AbacusFragment.kt
+++ app/src/main/java/ca/ulaval/ima/tp2/ui/abacus/AbacusFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import ca.ulaval.ima.tp2.R
+import com.google.android.material.slider.Slider
 
 class AbacusFragment : Fragment() {
 
@@ -15,7 +16,36 @@ class AbacusFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_slideshow, container, false)
-        // val textView: TextView = root.findViewById(R.id.text_slideshow)
+        val root = inflater.inflate(R.layout.fragment_abacus, container, false)
+
+        root.findViewById<Slider>(R.id.abacus_first_slider).addOnChangeListener { slider, value, fromUser ->
+            onChangeFirstSlider(root, value)
+        }
+
+        root.findViewById<Slider>(R.id.abacus_second_slider).addOnChangeListener { slider, value, fromUser ->
+            onChangeSecondSlider(root, value)
+        }
+
+        return root
+    }
+
+    fun setResultSlider(viewCtx: View, value: Float) {
+        viewCtx.findViewById<Slider>(R.id.abacus_result_slider).value = value
+        viewCtx.findViewById<TextView>(R.id.abacus_result_slider_value_label_placeholder).text = value.toInt().toString()
+    }
+
+
+    fun onChangeFirstSlider(viewCtx: View, value: Float) {
+        val secondSliderValue = viewCtx.findViewById<Slider>(R.id.abacus_second_slider).value
+
+        viewCtx.findViewById<TextView>(R.id.abacus_first_slider_value_label_placeholder).text = value.toInt().toString()
+        setResultSlider(viewCtx, value*secondSliderValue)
+    }
+
+    fun onChangeSecondSlider(viewCtx: View, value: Float) {
+        val firstSliderValue = viewCtx.findViewById<Slider>(R.id.abacus_first_slider).value
+
+        viewCtx.findViewById<TextView>(R.id.abacus_second_slider_value_label_placeholder).text = value.toInt().toString()
+        setResultSlider(viewCtx, value*firstSliderValue)
     }
 }
